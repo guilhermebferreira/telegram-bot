adadff93c2d7008f8447128aae3b2c3d36222112
Guilherme Ferreira <360guilherme@gmail.com>
2017-07-12 17:25:38 -0300

    starting telegram bot

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require( `node-telegram-bot-api` )
+
+require('dotenv').config() 
+
+const TOKEN = process.env.TOKEN
+
+const bot = new TelegramBot( TOKEN, { polling: true } )
+
+bot.on('message', function(msg){
+  console.log('msg', msg)
+})
