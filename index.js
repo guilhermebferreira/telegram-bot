d8cedaaf3b301aebd76ed63aff531d619c377a83
Guilherme Ferreira <360guilherme@gmail.com>
2017-07-12 17:42:03 -0300

    send 42 as answer to everything

--- index.js
+++ index.js
@@ -9,3 +9,31 @@ const bot = new TelegramBot( TOKEN, { polling: true } )
 bot.on('message', function(msg){
   console.log('msg', msg)
 })
+
+bot.onText( /(.*)\?/, function(msg, match){
+  bot.sendMessage( msg.chat.id, '42' )
+})
+
+var logErrorEcho = function logErrorEcho(msg) {
+  return function (err) {
+    return console.log(msg, err);
+  };
+};
+
+var logSuccessEcho = function(msg, match){
+  return function(data){
+    console.log( 'Success:', data);
+  };
+};
+
+var sendEcho = function(msg, match){
+  bot.sendMessage( msg.chat.id, match[ 1 ] )
+      .then( logSuccessEcho( msg, match ) )
+      .catch( logErrorEcho( 'Error:') );
+};
+
+var send42 = function(msg, match){
+  bot.sendMessage( msg.chat.id, '42' );
+};
+
+bot.onText( /\/echo (.*)/, sendEcho);
